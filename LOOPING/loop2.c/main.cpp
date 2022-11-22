8485b0612d14758c3b739f0258d992e8263bad9f
MFadhlanHkim <113012448+MFadhlanHkim@users.noreply.github.com>
2022-11-23 06:53:28 +0700

    Add files via upload

---
+++ LOOPING/loop2.c/main.cpp
@@ -0,0 +1,28 @@
+/*
+Nama : Muhammad Fadhlan Hakim
+Nim  : A11.2022.14619
+Kelas: Daspro
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+
+    for(int x=1; x<=100; x++)
+    {
+        if(x%2==0)
+        {
+            cout<< " Nilai Genap Adalah "<< x << endl;
+        }
+
+
+
+    }
+
+
+    return 0;
+}
---
+++ LOOPING/loop5.c/main.cpp
@@ -0,0 +1,30 @@
+/*
+Nama : Muhammad Fadhlan Hakim
+Nim  : A11.2022.14619
+Kelas: Daspro
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    while(true)
+    {
+        int x;
+        cout<< "Masukan Nilai Anda : ";cin>>x;
+        {
+            if(x==-99)
+            {
+                cout<<endl;
+                cout << "Keluar Karena Break" <<endl;
+                break;
+            }
+
+        }
+    }
+
+    return 0;
+}
